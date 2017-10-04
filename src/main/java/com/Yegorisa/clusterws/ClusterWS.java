c23bb580cda830d8910d5fb38eafb07729c7d492
Yegorisa <yegor1998@gmail.com>
2017-10-04 14:04:00 +1300

    Added documentation in ClusterWS.class and added getChannelByName method

--- src/main/java/com/Yegorisa/clusterws/ClusterWS.java
+++ src/main/java/com/Yegorisa/clusterws/ClusterWS.java
@@ -11,7 +11,11 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 /**
- * Created by Egor on 01.10.2017.
+ * ClusterWS.
+ * <p>
+ * <h3>Create ClusterWS</h3>
+ *
+ * @author Egor Egorov
  */
 
 public class ClusterWS {
@@ -22,6 +26,25 @@ public class ClusterWS {
     private BasicListener mBasicListener;
     private WebSocket mWebSocket;
     private ArrayList<Channel> mChannels;
+
+    /**
+     * <p></p>
+     * Returns new {@code ClusterWS} instance.
+     * <p>
+     *
+     * @param url                  the URI of the WebSocket endpoint on the server side. It must be provided.
+     * @param port                 the port of the WebSocket endpoint on the server side. Must be provided.
+     * @param autoReconnect        if you want to auto reconnect {@code true}, and {@code false} if you do not. Default false.
+     * @param reconnectionInterval the number of milliseconds between each reconnection attempt. Default 5000 milliseconds.
+     * @param reconnectionAttempts the number of reconnection attempts. Default 0.
+     *                             </p>
+     *                             <p>
+     * @throws NullPointerException the given URI is {@code null} or the port is {@code null}.
+     *                              </p>
+     *                              <p>
+     * @since 1.0
+     * </p>
+     */
 
     public ClusterWS(@NotNull String url, @NotNull Integer port, @Nullable Boolean autoReconnect, @Nullable Integer reconnectionInterval, @Nullable Integer reconnectionAttempts) {
         mEmitter = new Emitter();
@@ -55,7 +78,7 @@ public class ClusterWS {
                         send("#1", null, "ping");
                         return;
                     }
-                    Message.messageDecode(ClusterWS.this,message);
+                    Message.messageDecode(ClusterWS.this, message);
                 }
             });
         } catch (IOException e) {
@@ -67,9 +90,48 @@ public class ClusterWS {
         mWebSocket.sendText(Message.messageEncode(event, data, type));
     }
 
-    public void on(String event, Emitter.Listener fn) {
-        mEmitter.on(event, fn);
+    /**
+     * <p></p>
+     * Sets listener to receive event on this socket.
+     * <p>
+     *
+     * @param event    the name of the event which will be emitted from the server.
+     * @param listener a listener to add.
+     *                 </p>
+     *                 <p>
+     * @throws NullPointerException The given event name is {@code null}.
+     *                              </p>
+     *                              <p>
+     * @since 1.0
+     * </p>
+     */
+
+    public void on(@NotNull String event, Emitter.Listener listener) {
+        if (event == null) {
+            throw new NullPointerException("The event name must be provided");
+        }
+        mEmitter.on(event, listener);
     }
+
+    /**
+     * <p></P>
+     * Connect to the server with the provided url and port
+     * <p>
+     * If {@code connect()} failed,
+     * {@link BasicListener#onConnectError(ClusterWS, WebSocketException)} method is called.
+     * </P>
+     * <p>
+     * Note that this method can be called at most only once regardless of
+     * whether this method succeeded or failed. If you want to re-connect to
+     * the WebSocket endpoint, you have to create a new {@code WebSocket}
+     * instance again by  {@code new ClusterWS} constructor of a
+     * {@link ClusterWS}.
+     * </p>
+     * <p>
+     *
+     * @since 1.0
+     * </p>
+     */
 
     public void connect() {
         try {
@@ -79,13 +141,56 @@ public class ClusterWS {
         }
     }
 
+    /**
+     * <p></P>
+     * Executes {@link #connect()} asynchronous by creating a new thread and
+     * calling {@code connect()} in the thread.
+     * <p>
+     * If {@code connect()} failed,
+     * {@link BasicListener#onConnectError(ClusterWS, WebSocketException)}
+     * method is called.
+     * </p>
+     * <p>
+     *
+     * @since 1.0
+     * </p>
+     */
+
     public void connectAsynchronous() {
         mWebSocket.connectAsynchronously();
     }
 
+    /**
+     * <p></p>
+     * Sending message with data to a server
+     * <p></p>
+     *
+     * @param event the name of the event which will be emitted to the server.
+     * @param data  a data which will be sent to a server.
+     *              <p></p>
+     * @throws NullPointerException The given event name is {@code null}.
+     *                              <p></p>
+     * @since 1.0
+     */
+
     public void send(String event, Object data) {
+        if (event == null) {
+            throw new NullPointerException("Event name must be provided");
+        }
         mWebSocket.sendText(Message.messageEncode(event, data, "emit"));
     }
+
+    /**
+     * <p></p>
+     * Sending message with data to a server
+     * <p></p>
+     *
+     * @param channelName the name of the channel to subscribe.
+     *                    <p></p>
+     * @throws NullPointerException The given event name is {@code null}.
+     *                              <p></p>
+     * @since 1.0
+     */
 
     public Channel subscribe(String channelName) {
         for (Channel channel :
@@ -115,7 +220,17 @@ public class ClusterWS {
         return mChannels;
     }
 
-    Emitter getEmitter(){
+    public Channel getChannel(String channelName){
+        for (Channel channel:
+             mChannels) {
+            if (channel.getChannelName().equals(channelName)){
+                return channel;
+            }
+        }
+        return null;
+    }
+
+    Emitter getEmitter() {
         return mEmitter;
     }
 }
